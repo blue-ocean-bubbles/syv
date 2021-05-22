a1d2f742c5ec4af60ea4ccb40fdf2e84b1fb24ed
Dmanthonyx <dmanthonycoding@gmail.com>
2021-05-22 15:10:51 -0700

    update the user schema

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,11 @@
 import nextConnect from 'next-connect';
 import middleware from '../../server/middlewares';
-
+import User from '../../server/models/User';
 // create handler for middleware
 const handler = nextConnect();
 handler.use(middleware);
 handler.get(async (req, res) => {
+  // console.log(User.find({}) ,'user')
   return res.json({ message: 'Hitting this endpoint' })
 });
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
+let userSchema = mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 });
   
-const User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema);
 
-module.exports = User;
+mongoose.models = {};
+
+export default User;
