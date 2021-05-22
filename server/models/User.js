d05543e2894ef24a56d51a4822bfe06dbcf0393f
Dmanthonyx <dmanthonycoding@gmail.com>
2021-05-22 15:40:36 -0700

    added the address to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -5,8 +5,12 @@ let userSchema = mongoose.Schema({
     lastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    address: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    zipCode: {type: String, required: true},
 });
-  
+
 let User = mongoose.model('User', userSchema);
 
 mongoose.models = {};
