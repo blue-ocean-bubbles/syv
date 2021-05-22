b0d0b76b026f4288dac7147b28d89c35d21f9475
bhattibytes <bhattibytes@gmail.com>
2021-05-22 18:14:48 -0400

    Set up Landing Pages and routes for Sign-in Sign-up and Donate

---
+++ pages/sign-in.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Navbar from './navbar.js';
+import Footer from './footer.js';
+
+const SignIn = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default SignIn;
---
+++ pages/sign-up.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Navbar from './navbar.js';
+import Footer from './footer.js';
+
+const SignUp = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default SignUp;
