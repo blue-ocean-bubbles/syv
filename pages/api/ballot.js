c4a2d56c507f9599110545b3b035182594be0baf
Cody Mumford <cody.mumford@gmail.com>
2021-05-25 15:03:05 -0700

    added authentication to ballot submission

--- pages/api/ballot.js
+++ pages/api/ballot.js
@@ -2,6 +2,7 @@
 import nextConnect from 'next-connect';
 import middleware from '../../server/middlewares';
 import Tally from '../../server/models/Tally';
+
 // create handler for middleware
 const handler = nextConnect();
 handler.use(middleware);
@@ -9,6 +10,10 @@ handler.use(middleware);
 // POST api/ballot
 handler.post(async (req, res) => {
   try {
+    if (!req.user) {
+      res.status(401).json({ message: 'Unauthorized'});
+      throw 'exception';
+    }
     for (const vote of req.body) {
       await Tally.updateOne(vote, { $inc: { count: 1 } }, { upsert: true });
     }
