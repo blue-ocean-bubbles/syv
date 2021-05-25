fb5e53fb55c0fd9378d2a663eb687facc3e1e662
Dom Stepek <domstepek@gmail.com>
2021-05-25 10:24:03 -0700

    updated code

--- components/dashboard/election-card/contests-container.jsx
+++ components/dashboard/election-card/contests-container.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
+
+import cdcl from '../../../utils/conditional-classes';
 
 import GeneralContestCard from './general-contest-card';
 import ReferendumContestCard from './referendum-contest-card';
@@ -14,19 +17,19 @@ const ContestsContainer = ({ contests }) => {
       <div className="shadow-md">
         <div className="p-4 flex justify-between">
           <span className="text-lg">General Elections</span>
-          <span role="button" tabIndex={-1} onClick={() => setContestsExpanded(!contestsExpanded)}>vv</span>
+          <button className="btn-rounded btn-rounded-gray" tabIndex={-1} onClick={() => setContestsExpanded(!contestsExpanded)}>{`${contestsExpanded ? '^' : 'v'}`}</button>
         </div>
-        <div className={`transition-height mx-4 flex flex-wrap justify-center items-center gap-4 overflow-hidden${contestsExpanded ? ' p-4 h-full' : ' h-0'}`}>
+        <div className={`transition-height mx-4 flex flex-wrap justify-center items-center gap-4 overflow-hidden${cdcl(contestsExpanded, 'p-4 h-full', 'h-0')}`}>
           {contests.map((contest) => contest.type === 'General' && <GeneralContestCard contest={contest} key={contest.office} />)}
         </div>
       </div>
       <div className="shadow-md">
         <div className="p-4 flex justify-between">
           <span className="text-lg">Referendums</span>
-          <span role="button" tabIndex={-1} onClick={() => setReferendumsExpanded(!referendumsExpanded)}>vv</span>
+          <button className="btn-rounded btn-rounded-gray" onClick={() => setReferendumsExpanded(!referendumsExpanded)}>{`${referendumsExpanded ? '^' : 'v'}`}</button>
         </div>
-        <div className={`transition-height mx-4 flex flex-wrap justify-center items-center gap-4 overflow-hidden${referendumsExpanded ? ' p-4 h-full' : ' h-0'}`}>
-          {contests.map((contest) => contest.type === 'Referendum' && <ReferendumContestCard contest={contest} key={contest.office} />)}
+        <div className={`transition-height mx-4 flex flex-wrap justify-center items-center gap-4 overflow-hidden${cdcl(referendumsExpanded, 'p-4 h-full border-solid border-t-2 border-gray-600', 'h-0')}`}>
+          {contests.map((contest) => contest.type === 'Referendum' && <ReferendumContestCard contest={contest} key={contest.referendumTitle} />)}
         </div>
       </div>
     </div>
--- components/dashboard/election-card/referendum-contest-card.jsx
+++ components/dashboard/election-card/referendum-contest-card.jsx
@@ -6,7 +6,7 @@ const ReferendumContestCard = ({ contest }) => (
     <h1 className="text-lg font-bold px-4 group-hover:text-white">{contest.referendumTitle}</h1>
     <div className="px-4 pb-4 group-hover:text-white">
       <div>{contest.referendumSubtitle}</div>
-      <div>-&gt;</div>
+      <a href={contest.referendumUrl}>-&gt;</a>
     </div>
     <div className="mt-auto px-4 py-2 bg-gray-200 rounded-b-md group-hover:bg-gray-600 group-hover:text-white">
       {`(i) Source: ${contest.sources[0].name}, ${contest.sources[0].official ? 'Offical' : 'Unofficial'}`}
