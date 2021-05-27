73715ad5a8862c2bf9758e12cb3626906f1bade4
Marco Canchola <marcocanchola2020@gmail.com>
2021-05-26 20:46:26 -0700

    Refactored authenticated routes and user model * Implemented a user route to return user's info * Refactored user model schema * Implemented address check when signing up via 3rd party platforms

---
+++ pages/api/user.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+import nextConnect from 'next-connect';
+import middleware from '../../server/middlewares';
+import User from '../../server/models/User';
+
+
+// create handler for middleware
+const handler = nextConnect();
+handler.use(middleware);
+
+handler.get((req, res) => {
+  try {
+    // check to see if user is logged in
+    if (req.user) {
+      const { firstName, lastName, email, address } = req.user;
+      const user = { firstName, lastName, email, address };
+      res.json({ isLoggedIn: true, user: user });
+    } else {
+      // if not, return unauthorized error
+      res.status(401).json({ isLoggedIn: false, user: null });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// update user's address
+handler.put(async (req, res) => {
+  try {
+    // check to see if user is logged in
+    if (req.user) {
+      // get the address from request
+      const { state, street, city, zip } = req.body.address;
+      // get the user email
+      const { email } = req.user;
+      // find user via mongoose model and update address
+      await User.updateOne({ email: email }, { address: req.body.address });
+      // redirect user to dashboard
+      res.redirect('/dashboard').json({ message: 'Updated user\'s address.' });;
+    } else {
+      // if not, return unauthorized error
+      res.status(401).json({ message: 'Unauthorized to update user.' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed updating user address.' });
+  }
+});
+
+export default handler;
--- server/models/User.js
+++ server/models/User.js
@@ -3,19 +3,21 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 let userSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String},
-    address: {type: String},
-    city: {type: String},
-    state: {type: String},
-    zipCode: {type: String},
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    address: {
+        street: { type: String },
+        city: { type: String },
+        state: { type: String },
+        zip: { type: Number }
+    }
 });
 
 // custom user method to compare password
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
