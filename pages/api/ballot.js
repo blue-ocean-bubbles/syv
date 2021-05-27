9a630ab1322039d8d427f9f8674076ed5fb1a24f
Cody Mumford <cody.mumford@gmail.com>
2021-05-27 12:28:04 -0700

    updates user's hasVoted property when a user votes and prevents a user from voting twice

--- pages/api/ballot.js
+++ pages/api/ballot.js
@@ -1,23 +1,26 @@
 import nextConnect from 'next-connect';
 import middleware from '../../server/middlewares';
+import User from '../../server/models/User';
 import Tally from '../../server/models/Tally';
 
-// create handler for middleware
 const handler = nextConnect();
 handler.use(middleware);
 
-// POST api/ballot
 handler.post(async (req, res) => {
   try {
     if (!req.user) {
       res.status(401).json({ message: 'Unauthorized' });
       return;
     }
-    console.log(req.user);
-    // const votes = req.body.map((vote) => (
-    //   Tally.updateOne(vote, { $inc: { count: 1 } }, { upsert: true })
-    // ));
-    // await Promise.all(votes);
+    if (req.user.hasVoted) {
+      res.status(400).json({ message: 'user has already voted' });
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    await User.updateOne({ _id: req.user._id }, { hasVoted: true });
+    const votes = req.body.map((vote) => (
+      Tally.updateOne(vote, { $inc: { count: 1 } }, { upsert: true })
+    ));
+    await Promise.all(votes);
     res.status(201).json({ message: 'ballot counted' });
   } catch (err) {
     res.status(500).json({ message: 'something went wrong while counting ballot' });
