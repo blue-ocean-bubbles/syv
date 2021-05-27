fe91f4160b4cc66716897715dfc0739e2eade3c2
Roy Xue <roythegoose@gmail.com>
2021-05-27 12:38:16 -0700

    completed voter ballot functionality

--- pages/dashboard/ballot.jsx
+++ pages/dashboard/ballot.jsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable array-callback-return */
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-restricted-syntax */
+import React, { useState } from 'react';
 import axios from 'axios';
 import Confetti from 'react-confetti';
 import Dashboard from '../../components/dashboard';
@@ -6,65 +9,57 @@ import dummyData from '../../samples/ballotDummyData';
 import BallotAnimation from '../../components/dashboard/ballotAnimation';
 
 function Ballot() {
-  const height = 1100;
-  const width = 3000;
-  const [isSubmitted, changeSubmit] = useState(false);
+  const [animation, animate] = useState(false);
   const [ballot, setBallot] = useState({});
 
-  const handleCheck = (office, input, multipleAllowed = true, maxVotes = 3) => {
+  const handleCheck = (office, input, district) => {
     const newBallot = JSON.parse(JSON.stringify(ballot));
-    if (newBallot[office] === undefined) {
-      newBallot[office] = { check: [input] };
-    } else if (newBallot[office].check?.length < 3) {
-      newBallot[office].check.push(input);
-    } else {
-      newBallot[office].check = [input];
+    newBallot[office] = { check: input, id: district };
+    setBallot(newBallot);
+  };
+
+
+  const handleWriteIn = (office, input, district) => {
+    const newBallot = JSON.parse(JSON.stringify(ballot));
+    newBallot[office] = { writeIn: input, id: district };
+
+    if (newBallot[office].writeIn === '') {
+      delete newBallot[office];
     }
     setBallot(newBallot);
   };
 
-  const handleUncheck = (office, input) => {
+  const handleReferendum = (title, input, district) => {
+    // console.log('input', input);
     const newBallot = JSON.parse(JSON.stringify(ballot));
-    if (newBallot[office].check?.includes(input)) {
-      const index = newBallot[office].check.indexOf(input);
-      newBallot[office].check.splice(index, 1);
-    }
+    newBallot[title] = { vote: input, id: district };
     setBallot(newBallot);
   };
-
-  const handleWriteIn = (office, input) => {
-    const newBallot = JSON.parse(JSON.stringify(ballot));
-    if (newBallot[office] === undefined || newBallot[office].writeIn === undefined) {
-      newBallot[office] = { writeIn: input };
-    } else {
-      newBallot[office].writeIn = (input);
-    }
-    setBallot(newBallot);
-  };
-
-  // const alert = () => (<div style={{ zIndex: '50' }} role="alert"><Confetti width={1000} height={1000} /></div>);
-  // useEffect(() => );
 
   const handleSubmit = () => {
-    changeSubmit(true);
     const ballotArr = [];
+
+    for (const key of Object.keys(ballot)) {
+      if (ballot[key].check !== undefined) {
+        ballotArr.push({ name: ballot[key].check, contest: key, districtId: ballot[key].id });
+      }
+      if (ballot[key].writeIn !== undefined) {
+        ballotArr.push({ name: ballot[key].writeIn, contest: key, districtId: ballot[key].id });
+      }
+      if (ballot[key].vote !== undefined) {
+        ballotArr.push({ name: ballot[key].vote, contest: key, districtId: ballot[key].id });
+      }
+    }
+
+    console.log('ballot', ballotArr);
+    // axios.post('/api/ballot', ballotArr)
+    // .then(() => {
+    // animations
+    animate(true);
     setTimeout(() => {
-      changeSubmit(false);
-    }, 10000);
-
-    // for (const key in ballot) {
-    //   const ballotObj = {};
-    //   if (ballot[key].writeIn !== undefined) {
-    //     ballotObj.name = ballot[key]
-    //   }
-    // }
-
-    // axios.post('/api/ballot', ballot)
-    //   .then((response) => {
-    //     console.log('response', response);
-    //   });
-    // console.log('ballot', ballotArr);
-    // return (<Confetti width={width} height={height} />)
+      animate(false);
+    }, 15000);
+    // });
   };
 
   return (
@@ -73,54 +68,82 @@ function Ballot() {
         <div className="bg-gray-100 p-5">
           <h1 className="font-bold text-lg">{`${dummyData.election.name} Ballot`}</h1>
           <h2>{dummyData.election.electionDay}</h2>
-          <h2 className="">
+          <h2>
             Your Address:
             <br />
             {`${dummyData.normalizedInput.line1} ${dummyData.normalizedInput.city}, ${dummyData.normalizedInput.state} ${dummyData.normalizedInput.zip}`}
           </h2>
         </div>
 
+        {/* filter((contest) => contest.type === 'General') */}
+
         <div className="bg-gray-100 grid grid-cols-3 gap-x-5 gap-y-5 h-full p-5">
-          {dummyData.contests.filter((contest) => contest.type === 'General').map((contest, j) => (
-            <div key={j}>
-              <h1 className="font-bold">{contest.office}</h1>
-              <div>
-                {contest.candidates?.map((candidate, i) => (
-                  <React.Fragment key={i}>
-                    <div>{`${candidate.name}`}</div>
-                    <div className="text-right">
-                      {`${candidate.party} `}
-                      <input type="checkbox" onChange={(e) => (e.target.checked ? handleCheck(contest.office, candidate.name, true) : handleUncheck(contest.office, candidate.name))} />
-                    </div>
-                  </React.Fragment>
-                ))}
-                Write-in:
-                <input
-                  type="text"
-                  name="name"
-                  value={ballot[contest.office]?.writeIn || ''}
-                  onChange={(e) => handleWriteIn(contest.office, e.target.value)}
-                />
-              </div>
-            </div>
-          ))}
+          {dummyData.contests.map((contest, j) => {
+            if (contest.type === 'General') {
+              return (
+                <div key={j}>
+                  <h1 className="font-bold">{contest.office}</h1>
+                  <div>
+                    {contest.candidates?.map((candidate, i) => {
+                      return (
+                        <React.Fragment key={i}>
+                          <div>{`${candidate.name}`}</div>
+                          <div className="text-right">
+                            {`${candidate.party} `}
+                            <input
+                              type="radio"
+                              name={contest.office}
+                              onChange={() => (handleCheck(contest.office, candidate.name, contest.district.id))}
+                              checked={ballot[contest.office]?.check === candidate.name}
+                            />
+
+                          </div>
+                        </React.Fragment>
+                      );
+                    })}
+
+                    Write-in:
+                    <input
+                      type="text"
+                      name="name"
+                      value={ballot[contest.office]?.writeIn || ''}
+                      // eslint-disable-next-line max-len
+                      onChange={(e) => handleWriteIn(contest.office, e.target.value, contest.district.id)}
+                    />
+                  </div>
+                </div>
+              );
+            }
+            if (contest.type === 'Referendum') {
+              return (
+                <div key={j}>
+                  <h1 className="font-bold">{contest.referendumTitle}</h1>
+                  <label htmlFor="Yes">
+                    Yes
+                    <input id="Yes" name={contest.referendumTitle} type="radio" value="Yes" onClick={(e) => handleReferendum(contest.referendumTitle, e.target.value, contest.district.id)} />
+                  </label>
+
+                  <label htmlFor="No">
+                    No
+                    <input id="No" name={contest.referendumTitle} type="radio" value="No" onClick={(e) => handleReferendum(contest.referendumTitle, e.target.value, contest.district.id)} />
+                  </label>
+                </div>
+              );
+            }
+          })}
+
+          { /* Submit Button */}
           <br />
-          <button type="button" onClick={handleSubmit}>Submit</button>
-          {isSubmitted
-            ? (
-              <Confetti
-                width={document.documentElement.scrollWidth}
-                height={document.documentElement.scrollHeight}
-              />
-            )
-            : null}
-          {isSubmitted
-            ? (
-              <BallotAnimation />
+          <div className="border-4 text-center active:bg-red-700 border-red-100" onClick={handleSubmit}>Submit</div>
 
-            )
-            : null}
-
+          {/* Animation */}
+          {animation ? (
+            <Confetti
+              width={document.documentElement.scrollWidth}
+              height={document.documentElement.scrollHeight}
+            />
+          ) : null}
+          {animation ? <BallotAnimation /> : null}
 
         </div>
       </>
