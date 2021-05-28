17e32ec3f9af22543536eed9d1cef29ebf086e27
andrew lee <andrewjetlee777@gmail.com>
2021-05-27 22:50:54 -0700

    sign in progress

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -1,24 +1,52 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import axios from 'axios';
 
-const SignIn = () => (
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const SignIn = () => {
+  const [values, setValues] = useState(initialValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //check values at email
+    //check values at password for validation
+  }
+
+  const handleValidation = (e) => {
+    //check values at email
+    //check values at password for validation
+  }
+
+  return (
   <div>
     <Navbar>
       <div className="">
-        <form className="" style={{ zIndex: '50' }}>
+        <form className="" style={{ zIndex: '50' }} onSubmit={handleSubmit}>
           <label style={{ fontSize: '25px' }}>
             Email Address:
           {' '}
             <br />
-            <input type="text" name="name" className="" />
+            <input type="text" name="email" onChange={handleInputChange} className="" value={values.email}/>
           </label>
           <label style={{ fontSize: '25px' }}>
             Password:
           {' '}
             <br />
-            <input type="text" name="name" className="" />
+            <input type="text" name="password" onChange={handleInputChange} className="" value={values.password}/>
           </label>
           <input type="submit" value="Log-in" className="" />
         </form>
@@ -27,6 +55,7 @@ const SignIn = () => (
     <Footer />
 
   </div>
-);
+)
+};
 
 export default SignIn;
